3ff10f3dd7de1eeb4389e41d94bfd0fe8e4395e9
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-06-03 13:39:10 -0500

    changed logging level to WARNING

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 #logging config
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 #token input 
