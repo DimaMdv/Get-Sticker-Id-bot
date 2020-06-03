7e11912006ab1359e4db4f7d8160dc8b752cfbe2
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-06-03 13:28:22 -0500

    add stickerId function

--- bot.py
+++ bot.py
@@ -7,9 +7,15 @@ updater = Updater(token=TOKEN, use_context=True)
 def start(update, context):#/start command function
     context.bot.send_message(chat_id=update.effective_chat.id, text = "Hello! Send me a sticker and I'll send you it's id.")
 
+def stickerId(update, context): #get sticker id
+    context.bot.send_message(chat_id=update.effective_chat.id, text = "Here this sticker's id:")
+    # "update.message.sticker.file_id" its a sticker id
+    context.bot.send_message(chat_id=update.effective_chat.id, text = update.message.sticker.file_id)
 
 ''' HANDLERS '''
 updater.dispatcher.add_handler(CommandHandler('start', start))
+
+dispatcher.add_handler(MessageHandler(Filters.sticker, stickerId))
 
 updater.start_polling()
 updater.idle()
