502d6a37921785aaaab58e81b05b0c196b90d29f
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-06-03 13:20:38 -0500

    add /start command

--- bot.py
+++ bot.py
@@ -4,5 +4,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 TOKEN = input("Enter the token:")
 updater = Updater(token=TOKEN, use_context=True)
 
+def start(update, context):#/start command function
+    context.bot.send_message(chat_id=update.effective_chat.id, text = "Hello! Send me a sticker and I'll send you it's id.")
+
+
+''' HANDLERS '''
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
 updater.start_polling()
 updater.idle()
