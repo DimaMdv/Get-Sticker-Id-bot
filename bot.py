533337032bdd5f07aeaca780e1727c19bfa7dbf4
DimaTheCat <skr1ng4m1ng@gmail.com>
2020-06-03 13:38:20 -0500

    add error logging

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+#logging config
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 #token input 
 TOKEN = input("Enter the token:")
 updater = Updater(token=TOKEN, use_context=True)
+
 
 def start(update, context):#/start command function
     context.bot.send_message(chat_id=update.effective_chat.id, text = "Hello! Send me a sticker and I'll send you it's id.")
@@ -12,10 +17,17 @@ def stickerId(update, context): #get sticker id
     # "update.message.sticker.file_id" its a sticker id
     context.bot.send_message(chat_id=update.effective_chat.id, text = update.message.sticker.file_id)
 
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
 ''' HANDLERS '''
 updater.dispatcher.add_handler(CommandHandler('start', start))
 
 updater.dispatcher.add_handler(MessageHandler(Filters.sticker, stickerId))
+
+updater.dispatcher.add_error_handler(error)
+
 
 updater.start_polling()
 updater.idle()
